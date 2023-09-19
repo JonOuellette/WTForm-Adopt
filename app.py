d787c3ddce7c6cccb6e5a6bdc782926443368bf8
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-09-19 11:32:32 -0700

    Create Python files for app, forms and model.  Setup imports initial imports and config for app and model file.  Create Pet db model.

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from forms import
+from models import db, connect_db, Pet
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/adopt'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "petadopt123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+app.app_context().push()
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def home():
+
+    
---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Pet(db.Model):
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement= True)
+    pet_name = db.Column(db.Text, nullable = False )
+    species = db.Column(db.Text, nullable = False)
+    pet_image = db.Column(db.Text, nullable = True)
+    age = db.Column(db.Integer, nullable = True)
+    notes = db.Column(db.Text, nullable = True)
+    available = db.Column(db.Boolean, nullable = False, default = True)
+
+def connect_db(app):
+    """Connects database to Flask app"""
+    db.app = app
+    db.init_app(app)
